f348fe112e5319a95ee85137d607705a3e13e558
hian <hiancdtrsnm@gmail.com>
2020-02-05 19:47:23 -0500

    Add new configs

--- getlinks.py
+++ getlinks.py
@@ -1,7 +1,9 @@
 from parsel import Selector
 import requests
 import sys
-from pprint import pp
+from fire import Fire
+
+base_url = 'https://drive.google.com'
 
 def clean_url(url):
 
@@ -24,5 +26,22 @@ def get_links(url):
     return [clean_url(href) for href in all_href if 'drive.google.com' in href]
 
 
+def get_direct_download(url):
+    xpath_selector = '//*[@id="uc-download-link"]/@href'
+
+    ans = []
+
+    for file in get_links(url):
+        response = requests.get(file)
+
+        html = Selector(text=response.text)
+
+        href = html.xpath(xpath_selector).get()
+
+        ans.append(base_url + href)
+
+    return ans
+
+
 if __name__ == "__main__":
-    pp(get_links(sys.argv[1]))
+    Fire(get_links)
